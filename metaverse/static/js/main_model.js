90fbff49257ddf794aff36c715e3f0b24598c201
shum0415 <shum0415@gmail.com>
2021-11-09 22:44:04 +0900

    model prediction test

--- metaverse/static/js/main_model.js
+++ metaverse/static/js/main_model.js
@@ -12,7 +12,7 @@ async function init() {
     maxPredictions = model.getTotalClasses();
 
     // model load test
-    console.log("model load");
+    // console.log("model load");
 
     window.requestAnimationFrame(loop);
 }
@@ -28,22 +28,22 @@ async function loop() {
 async function predict() {
     const prediction = await model.predict(localVideo);
 
-    if(prediction[1].probability >= 0.2) {
-        console.log(prediction[1].className);
-    }
-    else if(prediction[0].probability >= 0.8) {
-        console.log(prediction[0].className);
-    }
-    else if(prediction[2].probability >= 0.4) {
-        console.log(prediction[2].className);
-    }
-    else if(prediction[3].probability >= 0.2) {
-        console.log(prediction[3].className);
-    }
-    else if(prediction[4].probability >= 0.3) {
-        console.log(prediction[4].className);
-    }
-    else console.log("none");
+//    if(prediction[1].probability >= 0.2) {
+//        console.log(prediction[1].className);
+//    }
+//    else if(prediction[0].probability >= 0.8) {
+//        console.log(prediction[0].className);
+//    }
+//    else if(prediction[2].probability >= 0.4) {
+//        console.log(prediction[2].className);
+//    }
+//    else if(prediction[3].probability >= 0.2) {
+//        console.log(prediction[3].className);
+//    }
+//    else if(prediction[4].probability >= 0.3) {
+//        console.log(prediction[4].className);
+//    }
+//    else console.log("none");
 
 //    console.log(prediction[0].probability);
     // for (let i = 0; i < maxPredictions; i++) {
