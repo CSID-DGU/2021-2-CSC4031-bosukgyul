09fe534f7f19cf201995c6ec131c14cc7fa7f18f
ygk313 <ygk313@naver.com>
2021-11-18 00:02:27 +0900

    ready와 main 연결

--- metaverse/static/js/ready.js
+++ metaverse/static/js/ready.js
@@ -1,13 +1,15 @@
 var localStream = new MediaStream();
 
 const constraints = {
-    'video' : true,
+    'video' :true,
     'audio': true,
 }
 
 const localVideo = document.querySelector('#local-video');
 const btnToggleAudio = document.querySelector('#btn-toggle-audio');
 const btnToggleVideo= document.querySelector('#btn-toggle-video');
+const AudioStatus = document.getElementById('#audio-status-check');
+const VideoStatus = document.getElementById('#video-status-check');
 
 var userMedia = navigator.mediaDevices.getUserMedia(constraints)
     .then(stream => {
@@ -22,25 +24,31 @@ var userMedia = navigator.mediaDevices.getUserMedia(constraints)
         videoTracks[0].enabled = true;
 
         btnToggleAudio.addEventListener('click', () => {
-            console.log(audioTracks);
             audioTracks[0].enabled = !audioTracks[0].enabled;
 
             if (audioTracks[0].enabled){
+                AudioStatus.value = '1';
                 btnToggleAudio.innerHTML = 'Audio Mute';
+                console.log(AudioStatus.value);
                 return;
             }
             btnToggleAudio.innerHTML = 'Audio UnMute';
+            AudioStatus.value='0';
+            console.log(AudioStatus.value);
         });
 
 
         btnToggleVideo.addEventListener('click', () => {
+            console.log(VideoStatus.value);
             videoTracks[0].enabled = !videoTracks[0].enabled;
 
             if (videoTracks[0].enabled){
+                VideoStatus.value = '1';
                 btnToggleVideo.innerHTML = 'Video OFF';
                 return;
             }
             btnToggleVideo.innerHTML = 'Video ON';
+            VideoStatus.value='0';
 
         });
     })
--- metaverse/zooming/views.py
+++ metaverse/zooming/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-
-def main_view(request):
-    return render(request, 'zooming/main.html')
+import pdb, json
 
 def ready(request):
     return render(request, 'zooming/ready.html')
 
 def landing(request):
     return render(request, 'zooming/landing.html')
+
+def meeting(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        teamname = request.POST.get('teamname')
+        videostatus = request.POST.get('video_status')
+        audiostatus = request.POST.get('audio_status')
+
+        context = {
+            'username' : username,
+            'teamname' : teamname,
+            'videoStatus' : videostatus,
+            'audioStatus' : audiostatus
+        }
+    # pdb.set_trace() 
+    return render(request, 'zooming/main.html', context)
