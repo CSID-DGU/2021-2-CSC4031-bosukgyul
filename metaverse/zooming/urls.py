cc7a03de3d5de7a7edda8cfcab2b8da01c9b0746
ygk313 <ygk313@naver.com>
2021-11-17 17:00:12 +0900

    url 변경

--- metaverse/zooming/urls.py
+++ metaverse/zooming/urls.py
@@ -5,5 +5,5 @@ app_name = 'zooming'
 
 urlpatterns = [
     path('ready/', ready, name="ready"),
-    path('landing/', landing, name="landing"),
+    path('meeting/', main_view, name="meeting"),
  ]
