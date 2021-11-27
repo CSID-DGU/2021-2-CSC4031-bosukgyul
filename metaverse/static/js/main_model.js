c10ff946a991bfc4478ec05d8cd8fa268f203622
shum0415 <shum0415@gmail.com>
2021-11-28 00:00:41 +0900

    ui modify

--- metaverse/static/js/main_model.js
+++ metaverse/static/js/main_model.js
@@ -41,19 +41,19 @@ async function predict() {
     if(prediction[1].probability >= 0.2) {
         predict_result = prediction[1].className;
         labelContainer.innerHTML = "Happy";
-        document.getElementById("emoticon").src = "../../../static/css/img/happy.jpg"
+        //document.getElementById("emoticon").src = "../../../static/css/img/happy.jpg"
         // console.log(predict_result);
     }
     else if(prediction[0].probability >= 0.8) {
         predict_result = prediction[0].className;
         labelContainer.innerHTML = "Neutral";
-        document.getElementById("emoticon").src = "../../../static/css/img/neutral.jpg"
+        //document.getElementById("emoticon").src = "../../../static/css/img/neutral.jpg"
         // console.log(predict_result);
     }
     else if(prediction[2].probability >= 0.4) {
         predict_result = prediction[2].className;
         labelContainer.innerHTML = "Surprise";
-        document.getElementById("emoticon").src = "../../../static/css/img/surprise.jpg"
+        //document.getElementById("emoticon").src = "../../../static/css/img/surprise.jpg"
         // console.log(predict_result);
     }
     else if(prediction[3].probability >= 0.2) {
